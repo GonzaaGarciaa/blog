a5540810c68b58422c5dbfad55d7ec43742482a3
Gonzalo Garcia <gonzaagarcia@hotmail.com>
2024-01-25 11:00:06 -0300

    Borrando el if de conf static

--- blog/settings.py
+++ blog/settings.py
@@ -137,9 +137,9 @@ REST_FRAMEWORK = {
 
 STATIC_URL = '/static/'
 
-if not DEBUG:    
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
